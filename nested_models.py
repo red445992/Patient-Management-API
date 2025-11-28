82265173fbcfcd39d61f9bb21da534b31fe03db5
aditya <thakuriaditya121@gmail.com>
2025-11-28 20:37:15 +0545

    pydantic learn

---
+++ nested_models.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    pin: str
+
+class Patient(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address: Address
+
+address_dict = {'city': 'kathmandu', 'state': 'bagmati', 'pin': '122001'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {'name': 'aditya', 'gender': 'male', 'age': 35, 'address': address1}
+
+patient1 = Patient(**patient_dict)
+
+print(patient1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Better organization of related data (e.g., vitals, address, insurance)
+
+# Reusability: Use Vitals in multiple models (e.g., Patient, MedicalRecord)
+
+# Readability: Easier for developers and API consumers to understand
+
+# Validation: Nested models are validated automatically—no extra work needed
